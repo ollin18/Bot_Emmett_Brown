d0afeda977d85e8fc8aa45c7238c7883152e6e1c
Ollin Langle <ollin.langle@ciencias.unam.mx>
2017-12-09 15:07:13 -0600

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build --tag botbrown:0.1 --tag botbrown:latest .
+docker build -f Dockerfile.bot --tag botbrown:0.1 --tag botbrown:latest .
 docker build -f Dockerfile.ment --tag botment:0.1 --tag botment:latest .
